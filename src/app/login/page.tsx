bb0e23242058ad8582962a3e5c54c2c28b43abf2
keanu-a <keanucaloua@gmail.com>
2025-05-07 18:24:22 -0700

    Configured login and sign up logic without google auth

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,17 @@
 'use client';
 
-import { useActionState } from 'react';
 import Link from 'next/link';
 
 import { cn } from '@/lib/utils';
 import { Button, buttonVariants } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import { ArrowLeftFromLineIcon, Loader2 } from 'lucide-react';
-import { signup } from './actions';
+import { useState } from 'react';
+import SignupForm from '@/components/SignupForm';
+import LoginForm from '@/components/LoginForm';
 
 export default function LoginPage() {
-  const [state, action, pending] = useActionState(signup, undefined);
+  const [mode, setMode] = useState<'login' | 'signup'>('signup');
+  const [pending, setPending] = useState<boolean>(false);
 
   return (
     <div className="relative w-screen h-screen flex flex-col justify-center items-center">
@@ -27,80 +27,15 @@ export default function LoginPage() {
       </Link>
 
       <div className="min-w-80 space-y-3">
-        <h1 className="text-2xl font-semibold tracking-tight">Sign Up</h1>
+        <h1 className="text-2xl font-semibold tracking-tight">
+          {mode === 'login' ? 'Login' : 'Sign Up'}
+        </h1>
 
-        {state?.message && (
-          <p className="text-ph-blue text-sm">{state?.message}</p>
+        {mode === 'signup' ? (
+          <SignupForm setPending={setPending} />
+        ) : (
+          <LoginForm setPending={setPending} />
         )}
-
-        <form className="space-y-4 *:space-y-1" action={action}>
-          <div>
-            <Label htmlFor="name">Name</Label>
-            <Input
-              id="name"
-              name="name"
-              placeholder="name"
-              autoCorrect="off"
-              required
-              defaultValue={state?.values?.name}
-            />
-          </div>
-          {state?.errors?.name && (
-            <p className="text-ph-red text-sm">{state.errors.name}</p>
-          )}
-
-          <div>
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              name="email"
-              placeholder="name@example.com"
-              type="email"
-              autoComplete="email"
-              autoCorrect="off"
-              required
-              defaultValue={state?.values?.email}
-            />
-          </div>
-          {state?.errors?.email && (
-            <p className="text-ph-red text-sm">{state.errors.email}</p>
-          )}
-
-          <div>
-            <Label htmlFor="password">Password</Label>
-            <Input
-              id="password"
-              name="password"
-              placeholder="password"
-              type="password"
-              required
-              defaultValue={state?.values?.password}
-            />
-          </div>
-          {state?.errors?.password && (
-            <div className="text-ph-red text-sm">
-              <p>Password must:</p>
-              <ul>
-                {state.errors.password.map((err, idx) => (
-                  <li key={idx}>- {err}</li>
-                ))}
-              </ul>
-            </div>
-          )}
-
-          {state?.errorMessage && (
-            <p className="text-ph-red text-sm">{state?.errorMessage}</p>
-          )}
-
-          <Button
-            type="submit"
-            className="rounded-full cursor-pointer w-full"
-            disabled={pending}
-          >
-            {pending ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : ''}
-            Sign Up
-          </Button>
-        </form>
 
         <div className="relative">
           <div className="absolute inset-0 flex items-center">
@@ -108,7 +43,7 @@ export default function LoginPage() {
           </div>
           <div className="relative flex justify-center text-xs uppercase">
             <span className="bg-background px-2 text-muted-foreground">
-              Or sign up with
+              Or {mode === 'login' ? 'login' : 'sign up'} with
             </span>
           </div>
         </div>
@@ -123,12 +58,27 @@ export default function LoginPage() {
           Google
         </Button>
 
-        <div className="text-center text-sm">
-          Already have an account?{' '}
-          <Link href="/login" className="text-ph-blue hover:underline">
-            Log in
-          </Link>
-        </div>
+        {mode === 'login' ? (
+          <div className="text-center text-sm">
+            Are you a new user?{' '}
+            <span
+              onClick={() => setMode('signup')}
+              className="text-ph-blue cursor-pointer hover:underline"
+            >
+              Sign up
+            </span>
+          </div>
+        ) : (
+          <div className="text-center text-sm">
+            Already have an account?{' '}
+            <span
+              onClick={() => setMode('login')}
+              className="text-ph-blue cursor-pointer hover:underline"
+            >
+              Login
+            </span>
+          </div>
+        )}
       </div>
     </div>
   );
